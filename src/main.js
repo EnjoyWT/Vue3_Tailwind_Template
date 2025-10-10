01c0bac78f10420a66af4205a8c3267a3e66b11f
wtz <wtz@sh.com>
2025-10-10 10:18:58 +0800

    12

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router'
-import pinia from './store/index'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "./router";
+import pinia from "./store/index";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
-const app = createApp(App)
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
 
-pinia.use(piniaPluginPersistedstate)
+const app = createApp(App);
 
-app.use(router)
-app.use(pinia)
-app.mount('#app')
+pinia.use(piniaPluginPersistedstate);
+
+app.use(router);
+app.use(pinia);
+app.use(ElementPlus);
+
+app.mount("#app");
